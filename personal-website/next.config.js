9804af51cd3a2db4b42a34cc44a2d8c9c38b21d6
Idan Levin <idanlevin23@gmail.com>
2025-03-12 13:19:16 +0200

    updates

--- personal-website/next.config.js
+++ personal-website/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  // Add trailing slash to help with routing
+  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
